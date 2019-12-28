9aaae48a9092ccf7ecd57ba07ce20eb04d38fd72
王春杉 <562932199@qq.com>
2019-12-28 20:27:07 +0800

    添加了后台首页的分页: 使用pagehelper插件

--- code/src/main/java/com/shan/tianmao/controller/CategoryController.java
+++ code/src/main/java/com/shan/tianmao/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.shan.tianmao.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.shan.tianmao.mapper.CategoryMapper;
 import com.shan.tianmao.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +20,15 @@ public class CategoryController {
 
     // Restful风格 查询全部
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        return categoryMapper.findAll();
+    public PageInfo<Category> list(
+            @RequestParam(value = "start", defaultValue = "0")int start,
+            @RequestParam(value = "size", defaultValue = "5")int size) throws Exception {
+        //根据start size 进行分页，并按照id倒序
+        PageHelper.startPage(start, size, "id desc");
+        List<Category> list = categoryMapper.findAll();
+        PageInfo<Category> page = new PageInfo<>(list);
+
+        return page;
     }
 
 
