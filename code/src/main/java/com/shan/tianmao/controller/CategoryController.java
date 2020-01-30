8b76c182f9a96ae79af6d0dc887ec8f5c810c1af
王春杉 <562932199@qq.com>
2020-01-30 12:25:57 +0800

    增加了 新增种类功能 和 文件上传

--- code/src/main/java/com/shan/tianmao/controller/CategoryController.java
+++ code/src/main/java/com/shan/tianmao/controller/CategoryController.java
@@ -4,12 +4,16 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.shan.tianmao.mapper.CategoryMapper;
 import com.shan.tianmao.pojo.Category;
+import com.shan.tianmao.util.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -29,6 +33,26 @@ public class CategoryController {
         PageInfo<Category> page = new PageInfo<>(list);
 
         return page;
+    }
+
+    @PostMapping("/categories")
+    public Object add(Category category, MultipartFile image,
+                      HttpServletRequest request)throws Exception {
+        categoryMapper.save(category);
+        saveOrUpdateImageFile(category, image, request);
+        return category;
+    }
+    public void saveOrUpdateImageFile(Category bean, MultipartFile image, HttpServletRequest request)
+            throws IOException {
+        File imageFolder= new File(request.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,bean.getId()+".jpg");
+        System.out.println(file.getPath());
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        image.transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
+        ImageIO.write(img, "jpg", file);
     }
 
 
