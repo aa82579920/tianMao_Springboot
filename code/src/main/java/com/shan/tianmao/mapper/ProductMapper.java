5701230b176df0887ed8508e99abf4a36bda05b0
王春杉 <562932199@qq.com>
2020-02-13 13:04:45 +0800

    添加了Product的实体类和Mapper类

---
+++ code/src/main/java/com/shan/tianmao/mapper/ProductMapper.java
@@ -0,0 +1,36 @@
+package com.shan.tianmao.mapper;
+
+import com.shan.tianmao.pojo.Category;
+import com.shan.tianmao.pojo.Product;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+
+    @Insert("insert into product (name, subTitle, originalPrice, promotePrice, stock, createDate, cid)" +
+            "values (#{name}, #{subTitle}, #{originalPrice}. #{promotePrice}, #{stock}, #{createDate}, #{category.id})")
+    int add(Product product);
+
+    @Delete("delete from product where id = #{id}")
+    void delete(int id);
+
+    @Update("update product set name=#{name}, subTitle=#{subTitle}, originalPrice=#{originalPrice},promotePrice=#{promotePrice},stock=#{stock},createDate=#{createDate},cid=#{category.id}" +
+            "where id=#{id}")
+    int update(Product product);
+
+    @Select("select * from product where id = #{id}")
+    @Results({
+            @Result(property = "category", column = "cid",
+                    one = @One(select = "com.shan.tianmao.mapper.CategoryMapper.get"))
+    })
+    int get(int id);
+
+    @Select("select * from product where cid=#{category.id}")
+    @Results({
+            @Result(property = "category", column = "cid",
+                    one = @One(select = "com.shan.tianmao.mapper.CategoryMapper.get"))
+    })
+    List<Product> findByCategory(@Param("category") Category category);
+}
---
+++ code/src/main/java/com/shan/tianmao/pojo/Product.java
@@ -0,0 +1,78 @@
+package com.shan.tianmao.pojo;
+
+import java.util.Date;
+
+public class Product {
+    private int id;
+    private Category category;
+    private String name;
+    private String subTitle;
+    private float  originalPrice;
+    private float promotePrice;
+    private int stock;
+    private Date createDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public float getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(float originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public float getPromotePrice() {
+        return promotePrice;
+    }
+
+    public void setPromotePrice(float promotePrice) {
+        this.promotePrice = promotePrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
